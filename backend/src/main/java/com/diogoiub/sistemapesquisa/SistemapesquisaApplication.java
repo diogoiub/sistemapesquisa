a2a6674c054ee856b1631f6595f3619ad4c77478
Diogo Cavalcante <diogoiub@outlook.com>
2020-09-19 10:45:52 -0300

    Project Created

---
+++ backend/src/main/java/com/diogoiub/sistemapesquisa/SistemapesquisaApplication.java
@@ -0,0 +1,13 @@
+package com.diogoiub.sistemapesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemapesquisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemapesquisaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/diogoiub/sistemapesquisa/SistemapesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diogoiub.sistemapesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemapesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
